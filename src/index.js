f7a065b9bd08f5b5683e48a21d438eb4f2bc6a9c
Eridani08 <dl566136@gmail.com>
2026-06-15 20:43:20 -0500

    switch to HashRouter for Render

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-
+import { HashRouter } from 'react-router-dom';  // 👈 Importa HashRouter
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>   {/* 👈 Envuelve tu App con HashRouter */}
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
 
